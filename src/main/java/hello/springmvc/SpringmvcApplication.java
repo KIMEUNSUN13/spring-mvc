c92a022a92bc33a0bdd59ee68d64ce62a288305d
eskim <eunsunkim13@gmail.com>
2022-06-05 16:42:56 +0900

    FEAT: Spring MVC 기본

--- src/main/java/hello/springmvc/SpringmvcApplication.java
+++ src/main/java/hello/springmvc/SpringmvcApplication.java
@@ -1,11 +1,18 @@
 package hello.springmvc;
 
+import hello.springmvc.basic.springstart.v1.SpringMemberFormControllerV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringmvcApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringmvcApplication.class, args);
 	}
+
+	/*@Bean
+	SpringMemberFormControllerV1 springMemberFormControllerV1() {
+		return new SpringMemberFormControllerV1();
+	}*/
 }
